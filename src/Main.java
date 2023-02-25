b207888a63e93a2da9f9729298c0baa9e577f632
aikezhanakhmatova <alinaakhmatova77@gmail.com>
2023-02-25 21:52:47 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,91 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        Scanner  nam  = new Scanner(System.in);
+//        int a = nam.nextInt();
+//        int s = nam.nextInt();
+//        int b = nam.nextInt();
+//        System.out.println(a+"+"+s+"+"+b+"="+(a+s+b));
+//        System.out.println(a+"*"+s+"*"+b+"="+(a*s*b));
+//        System.out.println(a+"+"+s+"+"+b+")"+"/3"+"="+(a+s+b)/3.);
+//
+
+
+        /// TODO   if else  ///
+
+//        Scanner scanner = new Scanner(System.in);
+//            int a = scanner.nextInt();
+//
+//            if(a>0){
+//                System.out.println("pozitiv");
+//            }else{
+//                System.out.println(" andaq can jok");
+//            }
+//
+//            Scanner scanner1 = new Scanner(System.in);
+//            int b = scanner1.nextInt();
+//
+//            if(0>b){
+//                System.out.println("negativ");
+//            }else {
+//                System.out.println(" tuura emes");
+//        }
+
+        /// TODO WHILE  ///
+//        Scanner scanner = new Scanner( System.in);
+//        int s = scanner.nextInt();
+//        while(s>0){
+//            s--;
+//            System.out.println(s);
+//        }
+
+
+        ///  MATH.POW   ///
+
+//        Scanner scanner = new Scanner(System.in);
+//        int b = scanner.nextInt();
+//        int s = scanner.nextInt();
+//        System.out.println(Math.pow(b,s));
+
+        ///  ODD DIGITS  ///
+
+//        Scanner b = new Scanner(System.in);
+//        int d = b.nextInt();
+//        while (d > 1) {
+//            d--;
+//            if (d % 2 == 1) {
+//                System.out.println(d);
+//            }
+
+
+        ///  FOR LOOP  ///
+
+//        Scanner scanner = new Scanner(System.in);
+//        byte a = scanner.nextByte();
+//        for (byte i = 0; i < 10; i++) {
+//            a--;
+//            if (a % 2 == 0) {
+//                System.out.println(a);
+//            }
+//        }
+
+
+     /// Miltiplication table FOOR LOOP  ///
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextByte();
+//        for (int i = 1; i < 10; i++) {
+//            {
+//                System.out.println(a + "*" + i + "=" + (a * i));
+//            }
+//
+//        }
+    }
+}
+
+
+
+
